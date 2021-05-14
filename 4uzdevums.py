4ae63146d6f7f350b7d3b04aa892bd049660f87b
RalfsMazurs <ralfs.mazurs@grobinasskola.lv>
2021-05-14 13:27:12 +0300

    Salaboju velvienu kludu

--- 4uzdevums.py
+++ 4uzdevums.py
@@ -10,4 +10,4 @@ Atgriežamā vērtība - Sarakstā ir 3 prasītie skaitļi
 saraksts = [2,6,2,32,2,17]
 
 number_count = saraksts.count(2)
-print('Ir sitik daudz 2 saraksta', number_count)
+print('Ir',number_count, 'divnieki saraksta.')
